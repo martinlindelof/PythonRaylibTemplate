b418d80edd58621d57a5c3b3b2712bb98aa672ab
Martin Lindelöf <martin.lindelof@gmail.com>
2025-09-22 07:36:54 +0200

    first template

---
+++ src/app.py
@@ -0,0 +1,32 @@
+import pyray as ray
+
+from engine import Engine
+
+TITLE = "App"
+SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 800
+
+
+class Application:
+    """The Application Singleton"""
+
+    ray.init_window(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
+    ray.set_target_fps(60)
+
+    engine: Engine
+
+    def __init__(self) -> None:
+        self.delta_time = 0.0
+        self.engine = Engine(app=self)
+
+    def run(self):
+        while not ray.window_should_close():
+            self.delta_time = ray.get_frame_time()
+            self.engine.update()
+            self.engine.draw()
+        self.exit()
+
+    def exit(self):
+        # Exit code here
+        self.engine.exit()
+        # Close window and un-load opengl context
+        ray.close_window()
---
+++ src/engine.py
@@ -0,0 +1,83 @@
+import pyray as ray
+
+
+class Engine:
+    camera3d = ray.Camera3D()
+    camera3d.position = ray.Vector3(10, 10, 10)
+    camera3d.target = ray.Vector3(0, 0, 0)
+    camera3d.up = ray.Vector3(0, 1, 0)
+    camera3d.projection = ray.CameraProjection.CAMERA_ORTHOGRAPHIC
+    camera3d.fovy = 12.0
+
+    def __init__(self, app) -> None:
+        self.app = app
+        #
+        self.load_fonts()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        ray.begin_drawing()
+        #
+        ray.clear_background(ray.WHITE)
+        self.debug_text()
+        self.draw_3d()
+        self.draw_2d()
+        #
+        ray.end_drawing()
+
+    def draw_2d(self):
+        pass
+
+    def draw_3d(self):
+        ray.begin_mode_3d(self.camera3d)
+        # grid
+        ray.draw_grid(20, 0.5)
+        # big orange cube
+        ray.draw_cube_v((0, 1, 0), (2, 2, 2), ray.ORANGE)
+        ray.draw_cube_wires_v((0, 1, 0), (2, 2, 2), ray.MAROON)
+        # small blue cube
+        ray.draw_cube_v((1.5, 0.5, 0.5), (1, 1, 1), ray.BLUE)
+        ray.draw_cube_wires_v((1.5, 0.5, 0.5), (1, 1, 1), ray.DARKBLUE)
+        #
+        ray.end_mode_3d()
+
+    def debug_text(self):
+        self.draw_text("Performance " + str(ray.get_fps()) + " fps", 0)
+        self.draw_text(
+            "Application Running " + str(ray.get_time())[0:5] + " seconds", 1
+        )
+        self.draw_text("AdwaitaMonoNerdFont_Regular_24", 2)
+        pass
+
+    DEBUG_LINEHEIGHT = 24
+
+    def draw_text(self, text: str, line: int = 0):
+        ray.draw_text_ex(
+            self.AdwaitaMonoNerdFont_Regular_24,
+            text,
+            (0, self.DEBUG_LINEHEIGHT * line),
+            24,
+            0,
+            ray.BLACK,
+        )
+
+    fonts_texture_filter = ray.TextureFilter.TEXTURE_FILTER_BILINEAR
+    AdwaitaMonoNerdFont_Regular_24: ray.Font
+
+    def load_fonts(self):
+        #
+        self.AdwaitaMonoNerdFont_Regular_24 = ray.load_font_ex(
+            "./resources/fonts/AdwaitaMono/AdwaitaMonoNerdFont-Regular.ttf",
+            24,
+            None,
+            250,
+        )
+        ray.set_texture_filter(
+            self.AdwaitaMonoNerdFont_Regular_24.texture, self.fonts_texture_filter
+        )
+        #
+
+    def exit(self):
+        ray.unload_font(self.AdwaitaMonoNerdFont_Regular_24)
