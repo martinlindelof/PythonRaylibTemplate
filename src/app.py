635aaec93d66b1da782a14d8048a8f60121d1790
Martin Lindelöf <martin.lindelof@gmail.com>
2025-09-25 09:31:46 +0200

    finnishing up Application singleton class

--- src/app.py
+++ src/app.py
@@ -9,14 +9,24 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 800
 class Application:
     """The Application Singleton"""
 
+    _instance: "Application | None" = None
+    _initialized: bool = False
+
     ray.init_window(WINDOW_WIDTH, WINDOW_HEIGHT, TITLE)
     ray.set_target_fps(60)
 
     engine: Engine
 
+    def __new__(cls) -> "Application":
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
     def __init__(self) -> None:
-        self.delta_time = 0.0
-        self.engine = Engine(app=self)
+        if not self._initialized:
+            self._initialized = True
+            self.delta_time = 0.0
+            self.engine = Engine(app=self)
 
     def run(self):
         while not ray.window_should_close():
